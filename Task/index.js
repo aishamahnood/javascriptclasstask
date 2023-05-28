b2f1ad401441b95a676e68b9caffdd97b94fbd09
aishamahnood <121711108+aishamahnood@users.noreply.github.com>
2023-05-28 04:21:54 -0700

    sign up
    sign up form

---
+++ Task/index.js
@@ -0,0 +1,9 @@
+function makeBig() {
+     document.getElementById("h1").className += " big";
+     }
+    //  function swapPic() {
+    //      document.getElementById("background.jpg").src = "backgroundafter.jpg";
+    //      }
+    function myFunction() {
+        document.getElementById("background").style.backgroundImage = "url(backgroundafter.jpg)";
+     }
